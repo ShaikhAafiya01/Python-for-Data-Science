5a2c91d0714f5f73ef311d096b212f4fbb1e1141
ShaikhAafiya01 <shaikhaafiyacs262703@gmail.com>
2026-08-20 13:20:26 +0530

    Add files via upload

---
+++ Practical no 7.py
@@ -0,0 +1,17 @@
+print("S128 Aafiya Shaikh")
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.facebook.com/"
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+print("Page Title:", soup.title.string)
+
+for heading in soup.find_all("h1"):
+    print("Heading:", heading.get_text())
+
+for link in soup.find_all("a", href=True)[:5]:
+    print("Link Text:", link.get_text(), "| URL:", link["href"])
